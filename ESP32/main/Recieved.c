67501d1f0f5d03a845c4890109df45c831a8df98
KimThanhNguyen-hcmus <nkimthanh47@gmail.com>
2026-06-18 20:41:47 +0700

    Update Webserver

--- ESP32/main/Recieved.c
+++ ESP32/main/Recieved.c
@@ -355,7 +355,6 @@ void wifi_init_sta(void)
 
     ESP_ERROR_CHECK(esp_netif_init());
     esp_netif_create_default_wifi_sta();
-
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
@@ -482,17 +481,19 @@ void app_main(void)
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
     wifi_init_sta();
     USART_Config();
     I2C_Config();
     OLED_Init();
     OLED_Clear();
     OLED_Update();
-    wifi_init_sta();
     start_webserver();
     while (1)
     {
         USART_Recieved();
         trigger_async_send(adc_val);
+        vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
